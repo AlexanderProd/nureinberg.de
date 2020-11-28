1c12fd9ab56b06e43d0c22989751b9cf87cac35b
Alexander Hörl <alexander.horl2@live.de>
2020-11-28 11:53:48 +0100

    added pflegeanleitung

---
+++ src/pages/pflegeanleitung.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Image from 'gatsby-image'
+import styled from '@emotion/styled'
+
+import Page from '~/templates/Page'
+import { breakpoints } from '~/utils/styles'
+
+const Icons = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-auto-flow: row dense;
+  gap: 0rem;
+
+  @media (max-width: ${breakpoints.l}px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Pflegenaleitung = ({ data }) => {
+  const { icons } = data
+
+  return (
+    <Page title="Pflegenaleitung">
+      <p>
+        Alle unsere Produkte bestehen aus 100% Bio Baumwolle. Dadurch findet
+        keine chemische Vorbehandlung statt, was dazu führt, dass bei der
+        Reinigung etwas vorsichtiger vorgegangen werden muss. Achten sie bitte
+        auf folgende Punkte:
+        <ul>
+          <li>max. 30° C Wassertemperatur</li>
+          <li>Vor dem Waschen umkrempeln</li>
+          <li>nur mit ähnliche Farben waschen</li>
+          <li>Feucht in Form bringen</li>
+          <li>Lufttrocknen / keinen Wäschetrockner</li>
+          <li>Kein Bleichmittel verwenden</li>
+        </ul>
+      </p>
+      <Icons style={{ margin: '10rem 0' }}>
+        {icons.edges.map(({ node }) => (
+          <Image
+            key={node.id}
+            alt={node.name}
+            fixed={node.childImageSharp.fixed}
+          />
+        ))}
+      </Icons>
+    </Page>
+  )
+}
+
+export default Pflegenaleitung
+
+export const query = graphql`
+  query {
+    icons: allFile(
+      filter: {
+        extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+        name: { regex: "/(waschsymbol)/" }
+      }
+    ) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            fixed(width: 100, height: 100) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
