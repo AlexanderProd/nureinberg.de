d583af4e50f00af4054358acac5867806b63246e
Alexander Hörl <alexander.horl2@live.de>
2020-11-29 18:46:02 +0100

    fixed confetti loading

--- src/pages/dankeschoen.js
+++ src/pages/dankeschoen.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { withPrefix } from 'gatsby'
+import React, { useEffect } from 'react'
 
 import Page from '~/templates/Page'
 
 const Dankeschoen = () => {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = '/confetti.js'
+    script.async = true
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <Helmet>
-        <script src={withPrefix('confetti.js')} type="text/javascript" />
-      </Helmet>
       <Page title="">
         <h1>Vielen Dank!</h1>
         <h2>Vom gesammten NurEinBerg Team</h2>
