20b962b1fc04068864917d6b46d8b512f363c06c
Alexander Hörl <alexander.horl2@live.de>
2020-12-03 09:48:19 +0100

    Update impressum.js

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -15,7 +15,7 @@ const Impressum = () => (
       90513 Zirndorf
       <br />
       <br />
-      Vertreten durch die Gesellschafter: Jens Herga, Kai Zippe, Anvar
+      Vertreten durch die Gesellschafter: Jens Herga, Alexander Hörl, Kai Zippe, Anvar
       Gallyaniev, Rabije Dzaferi, Ole Preisig
     </p>
     <h1>
