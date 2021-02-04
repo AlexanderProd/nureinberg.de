85767e6825d6fe20afaf07d55758c8589ea71674
Alexander Hörl <alexander.horl2@live.de>
2021-02-04 19:06:03 +0100

    text

--- src/components/ProductImages/index.js
+++ src/components/ProductImages/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
+import { GatsbyImage } from "gatsby-plugin-image";
 import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css'
 
-import { useWindowDimensions } from '~/utils/hooks'
-import { breakpoints, Img } from '~/utils/styles'
-
 const ProductImages = ({ product }) => {
-  const { images } = product
-  const { width } = useWindowDimensions()
-
   const properties = {
     duration: 5000,
     transitionDuration: 350,
@@ -19,28 +14,21 @@ const ProductImages = ({ product }) => {
 
   if (product.images) {
     return (
-      <div>
-        {width < breakpoints.l ? (
-          <Slide {...properties}>
-            {images.map(i => (
-              <Img
-                fluid={i.localFile.childImageSharp.fluid}
-                alt={product.title}
-                key={i.id}
-              />
-            ))}
-          </Slide>
-        ) : (
-          images.map(image => (
-            <Img
-              fluid={image.localFile.childImageSharp.fluid}
-              alt={product.title}
-              key={image.id}
-            />
-          ))
-        )}
+      <div className="slide-container">
+        <Slide {...properties}>
+          {product.images.map(i => (
+            <div className="each-fade" key={i.id}>
+              <div className="image-container">
+                <GatsbyImage
+                  image={i.localFile.childImageSharp.gatsbyImageData}
+                  alt={product.title}
+                  key={i.id} />
+              </div>
+            </div>
+          ))}
+        </Slide>
       </div>
-    )
+    );
   }
 }
 
