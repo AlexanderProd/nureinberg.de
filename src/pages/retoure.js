5f94ce64a1cfb6a2e308609a43a592302829fb12
Alexander Hörl <alexander.horl2@live.de>
2021-05-08 09:52:01 +0200

    text

--- src/pages/retoure.js
+++ src/pages/retoure.js
@@ -6,11 +6,15 @@ const kontakt = () => (
   <Page title="Retoure">
     <p>
       Sollte dein NurEinBerg Shirt nicht passen oder dir nicht gefallen, kannst
-      du es bis 30 Tage nach Zustellung zurückgeben.
+      du es einfach bis 30 Tage nach Zustellung zurückgeben.
       <br />
       Schicke uns dazu einfach eine Email mit deiner Bestellnummer an:
     </p>
     <a href="mailto:mail@nureinberg.de">info@nureinberg.de</a>
+    <p>
+      Dann schickst du uns das Shirt wieder zurück und wir erstatten dir deinen
+      Rechnungsbetrag.
+    </p>
   </Page>
 )
 
