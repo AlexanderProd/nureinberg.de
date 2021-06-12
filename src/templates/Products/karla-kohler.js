7a6076f41d7716595dd8c5e2c03b6a225600a94d
Alexander Hörl <alexander.horl2@live.de>
2021-06-12 18:17:36 +0200

    published karla köhler page

--- src/templates/Products/karla-kohler.js
+++ src/templates/Products/karla-kohler.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import styled from '@emotion/styled'
+import { StaticImage } from 'gatsby-plugin-image'
 
 import Seo from '~/components/seo'
 import Navigation from '~/components/Navigation'
@@ -13,7 +14,8 @@ import {
   TwoColumnGrid,
   MainContent,
   breakpoints,
-} from '~/utils/styles'
+} from '../../utils/styles'
+import kkAnimation from '~/images/kk_animation.mp4'
 
 const HeroWrapper = styled.div`
   text-align: center;
@@ -32,8 +34,8 @@ const HeroWrapper = styled.div`
   }
 `
 
-const HeroImage = styled.img`
-  width: 50%;
+const HeroVideo = styled.video`
+  width: 60%;
 
   @media (max-width: ${breakpoints.l}px) {
     width: 100%;
@@ -52,11 +54,6 @@ const H2 = styled.h2`
 const Text = styled.p`
   line-height: 1.6;
   font-size: 1.2rem;
-`
-
-const TextWrapper = styled.div`
-  text-align: center;
-  max-width: 800px;
 `
 
 const ProductTitle = styled.h1`
@@ -87,6 +84,50 @@ const KarlaKöhlerShirtPage = ({ data }) => {
       <Navigation />
       <Container>
         <MainContent>
+          <HeroWrapper>
+            <HeroVideo muted playsInline autoPlay>
+              <source src={kkAnimation} type="video/mp4" />
+            </HeroVideo>
+            <H2>Karla Köhler X NurEinBerg</H2>
+            <Text>
+              Für NurEinBerg ist Mode ein Teil der Kunst. So war es schon immer
+              der Anspruch der Marke, zeitgenössische Kunst für den Alltag der
+              Menschen zugänglich zu machen. Unsere Motive sind minimalistisch
+              Interpretationen der Stadt Nürnberg, wie auch bei den Kunstwerken
+              der Künstlerin Karla Köhler.
+              <br />
+              <br />
+              So ergab sich eine Zusammenarbeit die nicht nur aufgrund gleicher
+              Stilrichtungen entstand. Wie auch NurEinBerg stammt Karla Köhler
+              aus Nürnberg, weshalb viele Ihrer Kunstwerke einen regionalen
+              Bezug haben.
+            </Text>
+          </HeroWrapper>
+
+          <TwoColumnGrid>
+            <Text>
+              Karla Köhler studierte Malerei und Bildhauerei, was auch in Ihren
+              Arbeiten zu erkennen ist. Als Grundlage dient meist eine
+              konturierte Leinwand, welche mit einem Dreidimensionalen Objekt
+              aus verschiedensten Materialien veredelt wird. So werden
+              beispielsweise Holz, Plastik oder Metal mit modernsten Techniken
+              wie Laserschneiden und 3D Druck zum Einsatzgebracht um Ihre Werke
+              zu vollenden. Ihre aktuelle Themenreihe sind die Urbanen Welten,
+              bei denen Stadtansichten minimalistisch wiedergegeben werden.
+              Durch einfache Handzeichnungen in den einzelnen Städten kommt es
+              zur Entwicklung des Bildes, welches dann als Vektorgrafik
+              weiterverarbeitet wird. Ihre Arbeiten sind zu sehen in ihrem
+              Atelier KUNSTKELLER hier im Defethaus (Termine können per Mail
+              vereinbart werden) sowie im raum für zeitgenössische kunst von
+              Laurentiu Feller.
+            </Text>
+            <StaticImage
+              src="../../images/kk_portrait.jpg"
+              layout="fullWidth"
+              alt="Karla Köhler Portrait"
+            />
+          </TwoColumnGrid>
+
           <TwoColumnGrid style={{ margin: '10rem 0' }}>
             <ProductImages product={product} />
             <div>
